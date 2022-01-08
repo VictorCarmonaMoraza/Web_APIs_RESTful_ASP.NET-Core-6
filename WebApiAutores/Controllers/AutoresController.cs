495b38e68332bb9bbdb633b412ce01dc1fd3d2b3
Victor Carmona <VictorCarmona9035@hotmail.com>
2022-01-08 12:37:34 +0100

    22-Lyendo y creando recursos desde el Controlador

--- WebApiAutores/Controllers/AutoresController.cs
+++ WebApiAutores/Controllers/AutoresController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,26 +12,38 @@ namespace WebApiAutores.Controllers
     [Route("api/autores")]
     public class AutoresController : ControllerBase
     {
+        private readonly ApplicationDbContext context;
+
+        //Constructor
+        public AutoresController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+
         /// <summary>
         /// Devolvemos un listado de Autores que estan creados en memoria
         /// </summary>
         /// <returns></returns>
         [HttpGet]
-        public ActionResult<List<Autor>> Get()
+        public async Task<ActionResult<List<Autor>>> Get()
         {
-            return new List<Autor>()
-            {
-                new Autor()
-                {
-                    Id = 1,
-                    Nombre = "Felipe"
-                },
-                new Autor()
-                {
-                    Id = 2,
-                    Nombre = "Claudia"
-                }
-            };
+            //Obtenemos el listado de autores de la base de datos
+            return await context.Autores.ToListAsync();
+        }
+
+        /// <summary>
+        /// Creacion de un autor
+        /// </summary>
+        /// <param name="autor">modelo de autor</param>
+        /// <returns></returns>
+        [HttpPost]
+        public async Task<ActionResult>Post(Autor autor)
+        {
+            //Creamos un autor en la base de datos
+            context.Add(autor);
+            await context.SaveChangesAsync();
+            return Ok();
         }
     }
 }
