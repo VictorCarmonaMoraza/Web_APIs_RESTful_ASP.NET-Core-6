ef3a5f1c4c63e073247917f09c9d8c6cf7c5e83a
Victor Carmona <VictorCarmona9035@hotmail.com>
2022-01-08 12:22:32 +0100

    21-Instalando Entity Framework Core y creando la base de datos

---
+++ WebApiAutores/DbContext/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApiAutores.Entidades;
+
+namespace WebApiAutores
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        //Tablas que vamos a crear en la base de datos 
+        public DbSet<Autor> Autores { get; set; }
+    }
+}
